72985188077f84f4f7ace9e3e737fcb6bb82758a
danfilof <danila.anfilofyev@gmail.com>
2024-02-26 12:14:11 +0100

    Testing

--- src/main/java/com/anfilofyev/routes/RouteThree.java
+++ src/main/java/com/anfilofyev/routes/RouteThree.java
@@ -9,12 +9,6 @@ import java.util.List;
 
 public class RouteThree extends Route {
 
-    /*List<StationsEnum> stationList = new ArrayList<>(EnumSet.of(StationsEnum.BirstfeldenHard, StationsEnum.Salinenstr, StationsEnum.Schulstr, StationsEnum.Baren, StationsEnum.Breite,
-            StationsEnum.Waldenburgerstr, StationsEnum.StAlbanTor, StationsEnum.Aeschenplatz, StationsEnum.Bankverein, StationsEnum.Barfusserplatz, StationsEnum.MusikAkademie,
-            StationsEnum.Universitat, StationsEnum.Spalentor, StationsEnum.Pilgerstr, StationsEnum.Burgfelderplatz, StationsEnum.Strassburgerallee, StationsEnum.FelixPlatterSpital,
-            StationsEnum.Luzernerring, StationsEnum.WaldighoferStr, StationsEnum.Burgfelderhof, StationsEnum.SaintExupery, StationsEnum.PlaceMermoz, StationsEnum.Soleil,
-            StationsEnum.SaintLouisGare));*/
-
     public RouteThree() {
         super(3, generateStationList(), StationsEnum.BirstfeldenHard, StationsEnum.SaintLouisGare);
     }
